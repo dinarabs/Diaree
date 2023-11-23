5aea68dfb27b0ad78d3f3e6afc7d53ef6f7c40b1
PilySwatch <pilar.cuellarta@gmail.com>
2023-11-23 10:57:31 +0100

    refactor: elements within EntryCard component && seedDB code in Server

--- client-ts/src/components/EntryCard.tsx
+++ client-ts/src/components/EntryCard.tsx
@@ -19,36 +19,40 @@ const EntryCard: FC<EntryCardProps> = ({ diaryEntry, onDelete }) => {
     diaryEntry.text.length > 25
       ? `${diaryEntry.text.substring(0, 25)}...`
       : diaryEntry.text
+      
 
   return (
-    <div className="flex items-start justify-between shadow-md w-full mx-0 my-4 h-32 p-4 border-2 border-light-grey rounded-lg">
-      <img src={diaryEntry.imageUrl} alt="Image of the diary entry" className='object-scale-down h-24 w-24 bg-black'/>
-      <div className="flex-shrink-0 flex flex-col ">
-        <Link to="/entry" className="no-underline hover:underline ">
-          <h2 className="text-xl font-bold">{diaryEntry.title}</h2>
-        </Link>
-        <h5 className="text-sm">{truncatedText}</h5>
-        <div className="my-2">
-          {diaryEntry.tags.map((tag, index) => (
-            <div
-              key={index}
-              className={`inline-flex flex-wrap w-fit bg-green opacity-75 text-white rounded-full px-2 py-1 text-sm mr-3`}
-            >
-              {tag}
-            </div>
-          ))}
-        </div>
-      </div>
+    <div className="flex w-full rounded overflow-hidden shadow-md border-light-grey my-4">
+      <img src={diaryEntry.imageUrl} alt="Image of the diary entry" className='h-32 w-32 object-fill scale-100 mr-4'/>
+      <div className='flex justify-between w-full'>
 
-      <div className="flex flex-col items-end gap-y-6 ">
-        <UilTimes
-          size={32}
-          onClick={handleDeleteClick}
-          style={{ cursor: 'pointer' }}
-        />
-        <h5 className="text-sm pt-4 text-grey w-fit">
-          {moment.utc(diaryEntry.createdAt).format('MMMM Do, YYYY - h:mm a ')}
-        </h5>
+        <div className="p-2">
+          <Link to="/entry" className="no-underline hover:underline ">
+            <h2 className="text-xl font-bold">{diaryEntry.title}</h2>
+          </Link>
+          <h5 className="text-sm">{truncatedText}</h5>
+          <div className="my-2">
+            {diaryEntry.tags.map((tag, index) => (
+              <div
+                key={index}
+                className={`inline-flex flex-wrap w-fit bg-green opacity-75 text-white rounded-full px-2 py-1 text-sm mr-3`}
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="p-4 flex flex-col items-end justify-between">
+          <UilTimes
+            size={32}
+            onClick={handleDeleteClick}
+            style={{ cursor: 'pointer' }}
+          />
+          <h5 className="text-sm pt-4 text-grey w-fit">
+            {moment.utc(diaryEntry.createdAt).format('MMMM Do, YYYY - h:mm a ')}
+          </h5>
+        </div>
       </div>
     </div>
   )
--- server/utils/seedDiaryEntriesDB.js
+++ server/utils/seedDiaryEntriesDB.js
@@ -4,20 +4,35 @@ var mongoose_1 = require("mongoose");
 var db_1 = require("../models/db");
 var fs = require("fs");
 var diaryEntry_1 = require("../models/diaryEntry");
+var tag_1 = require("../models/tag"); // Assuming you have a Tag model
 (0, db_1.default)();
 fs.readFile('./sampleEntries.json', 'utf8', function (err, data) {
     if (err) {
-        console.error('Error reading file:', err);
+        console.error('Error reading DiaryEntries file:', err);
         return;
     }
-    var jsonData = JSON.parse(data);
-    diaryEntry_1.default.insertMany(jsonData)
+    var diaryData = JSON.parse(data);
+    diaryEntry_1.default.insertMany(diaryData)
         .then(function () {
         console.log('DiaryEntries seeded successfully✨');
-        mongoose_1.default.connection.close();
     })
         .catch(function (error) {
         console.error('Error seeding DiaryEntries:', error);
+    });
+});
+fs.readFile('./sampleTags.json', 'utf8', function (err, data) {
+    if (err) {
+        console.error('Error reading Tags file:', err);
+        return;
+    }
+    var tagData = JSON.parse(data);
+    tag_1.default.insertMany(tagData)
+        .then(function () {
+        console.log('Tags seeded successfully✨');
+        mongoose_1.default.connection.close();
+    })
+        .catch(function (error) {
+        console.error('Error seeding Tags:', error);
         mongoose_1.default.connection.close();
     });
 });
